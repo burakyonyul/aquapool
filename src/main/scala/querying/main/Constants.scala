a290b7b8734ce55e82b22c74299db97710701168
burakyonyul <burakyonyul@gmail.com>
2023-06-15 17:35:29 +0300

    except procedures_icd and diagnoses_icd all transformers have been completed

--- src/main/scala/querying/main/Constants.scala
+++ src/main/scala/querying/main/Constants.scala
@@ -3,6 +3,11 @@ package querying.main
 import com.hp.hpl.jena.rdf.model.{Property, Resource, ResourceFactory}
 
 object Constants {
+  val DIAGNOSE_ICD_QUERY: String = ???
+
+  val PROCEDURE_ICD_QUERY: String = ???
+
+
   val ACTOR_COUNT = "actor-count"
   val QUERY_COUNT = "query-count"
   val Kilobytes = 1024L
@@ -16,9 +21,12 @@ object Constants {
   val MIMIC_ONTOLOGY_URI: String = MIMIC_BASE_URI + "ontology/"
   val MIMIC_RESOURCE_URI: String = MIMIC_BASE_URI + "resource/"
 
-  val MIMIC_D_ITEM_URI: String = MIMIC_ONTOLOGY_URI + "D_Item"
+  val MIMIC_D_ITEM_CLS: Resource = ResourceFactory.createResource(MIMIC_ONTOLOGY_URI + "D_Item")
+  val MIMIC_D_LAB_ITEM_CLS: Resource = ResourceFactory.createResource(MIMIC_ONTOLOGY_URI + "D_Lab_Item")
+  val MIMIC_D_ICD_PROCEDURE_CLS: Resource = ResourceFactory.createResource(MIMIC_ONTOLOGY_URI + "D_Icd_Procedure")
+  val MIMIC_D_ICD_DIAGNOSE_CLS: Resource = ResourceFactory.createResource(MIMIC_ONTOLOGY_URI + "D_Icd_Diagnose")
+  val MIMIC_CAREGIVER_CLS: Resource = ResourceFactory.createResource(MIMIC_ONTOLOGY_URI + "Caregiver")
 
-  val MIMIC_D_ITEM: Resource = ResourceFactory.createResource(MIMIC_D_ITEM_URI)
   val ITEM_ID_PRP: Property = ResourceFactory.createProperty(MIMIC_ONTOLOGY_URI + "itemid")
   val LABEL_PRP: Property = ResourceFactory.createProperty(MIMIC_ONTOLOGY_URI + "label")
   val ABBREVIATION_PRP: Property = ResourceFactory.createProperty(MIMIC_ONTOLOGY_URI + "abbreviation")
@@ -28,5 +36,72 @@ object Constants {
   val UNIT_NAME_PRP: Property = ResourceFactory.createProperty(MIMIC_ONTOLOGY_URI + "unitname")
   val PARAM_TYPE_PRP: Property = ResourceFactory.createProperty(MIMIC_ONTOLOGY_URI + "paramtype")
   val CONCEPT_ID_PRP: Property = ResourceFactory.createProperty(MIMIC_ONTOLOGY_URI + "conceptid")
+  val FLUID_PRP: Property = ResourceFactory.createProperty(MIMIC_ONTOLOGY_URI + "fluid")
+  val LOINC_CODE_PRP: Property = ResourceFactory.createProperty(MIMIC_ONTOLOGY_URI + "loinc_code")
+  val ICD_9_CODE_PRP: Property = ResourceFactory.createProperty(MIMIC_ONTOLOGY_URI + "icd9_code")
+  val LONG_TITLE_PRP: Property = ResourceFactory.createProperty(MIMIC_ONTOLOGY_URI + "long_title")
+  val SHORT_TITLE_PRP: Property = ResourceFactory.createProperty(MIMIC_ONTOLOGY_URI + "short_title")
+  val DESCRIPTION_PRP: Property = ResourceFactory.createProperty(MIMIC_ONTOLOGY_URI + "description")
+  val CGID_PRP: Property = ResourceFactory.createProperty(MIMIC_ONTOLOGY_URI + "cgid")
+  val HADM_ID_PRP: Property = ResourceFactory.createProperty(MIMIC_ONTOLOGY_URI + "hadm_id")
+  val SUBJECT_ID_PRP: Property = ResourceFactory.createProperty(MIMIC_ONTOLOGY_URI + "subject_id")
+
+  val D_ITEM_QUERY: String =
+    s"""
+       |PREFIX mimic-rsc:<${MIMIC_RESOURCE_URI}>
+       |PREFIX mimic-ont:<${MIMIC_ONTOLOGY_URI}>
+       |SELECT * WHERE {
+       |?d_item mimic-ont:itemid ?itemid.
+       |?d_item mimic-ont:label ?label.
+       |?d_item mimic-ont:abbreviation ?abbreviation.
+       |?d_item mimic-ont:dbsource ?dbsource.
+       |?d_item mimic-ont:linksto ?linksto.
+       |?d_item mimic-ont:category ?category.
+       |?d_item mimic-ont:unitname ?unitname.
+       |?d_item mimic-ont:paramtype ?paramtype.
+       |?d_item mimic-ont:conceptid ?conceptid.
+       |}""".stripMargin
+
+  val D_LAB_ITEM_QUERY: String =
+    s"""
+       |PREFIX mimic-rsc:<${MIMIC_RESOURCE_URI}>
+       |PREFIX mimic-ont:<${MIMIC_ONTOLOGY_URI}>
+       |SELECT * WHERE {
+       |?d_lab_item mimic-ont:itemid ?itemid.
+       |?d_lab_item mimic-ont:label ?label.
+       |?d_lab_item mimic-ont:fluid ?fluid.
+       |?d_lab_item mimic-ont:category ?category.
+       |?d_lab_item mimic-ont:loinc_code ?loinc_code.
+       |}""".stripMargin
+
+  val D_ICD_PROCEDURE_QUERY: String =
+    s"""
+       |PREFIX mimic-rsc:<${MIMIC_RESOURCE_URI}>
+       |PREFIX mimic-ont:<${MIMIC_ONTOLOGY_URI}>
+       |SELECT * WHERE {
+       |?d_icd_procedure mimic-ont:icd9_code ?icd9_code.
+       |?d_icd_procedure mimic-ont:short_title ?short_title.
+       |?d_icd_procedure mimic-ont:long_title ?long_title.
+       |}""".stripMargin
+
+  val D_ICD_DIAGNOSE_QUERY: String =
+    s"""
+       |PREFIX mimic-rsc:<${MIMIC_RESOURCE_URI}>
+       |PREFIX mimic-ont:<${MIMIC_ONTOLOGY_URI}>
+       |SELECT * WHERE {
+       |?d_icd_diagnose mimic-ont:icd9_code ?icd9_code.
+       |?d_icd_diagnose mimic-ont:short_title ?short_title.
+       |?d_icd_diagnose mimic-ont:long_title ?long_title.
+       |}""".stripMargin
+
+  val CAREGIVER_QUERY: String =
+    s"""
+       |PREFIX mimic-rsc:<${MIMIC_RESOURCE_URI}>
+       |PREFIX mimic-ont:<${MIMIC_ONTOLOGY_URI}>
+       |SELECT * WHERE {
+       |?caregiver mimic-ont:cgid ?cgid.
+       |?caregiver mimic-ont:label ?label.
+       |?caregiver mimic-ont:description ?description.
+       |}""".stripMargin
 
 }
--- src/main/scala/querying/transformation/RedisTransformer.scala
+++ src/main/scala/querying/transformation/RedisTransformer.scala
@@ -2,101 +2,121 @@ package querying.transformation
 
 import com.hp.hpl.jena.datatypes.xsd.XSDDatatype
 import com.hp.hpl.jena.query.QueryExecutionFactory
-import com.hp.hpl.jena.rdf.model.{ModelFactory, ResourceFactory}
+import com.hp.hpl.jena.rdf.model.{Model, ModelFactory, ResourceFactory}
 import com.hp.hpl.jena.vocabulary.RDF
 import querying.main.{Constants, QueryingUtils}
 import querying.message.Result
 
 object RedisTransformer {
 
+
   /**
    * ITEMID --> [LABEL, ABBREVIATION, DBSOURCE, LINKSTO, CATEGORY, UNITNAME, PARAM_TYPE, CONCEPTID]
    */
-  def transform_d_items(resultMap: Map[String, Option[Any]]): Option[Result] = {
-    val model = ModelFactory.createDefaultModel()
-    for ((key, redisResult) <- resultMap) {
-      val propertyValueList = redisResult.asInstanceOf[Option[List[Option[String]]]]
-      val itemRsc = ResourceFactory.createResource(Constants.MIMIC_RESOURCE_URI + "d_item/" + key)
-      val itemIdValue = key
-      val labelValue = propertyValueList.get.head
-      val abbreviationValue = propertyValueList.get(1)
-      val dbsourceValue = propertyValueList.get(2)
-      val linkstoValue = propertyValueList.get(3)
-      val categoryValue = propertyValueList.get(4)
-      val unitnameValue = propertyValueList.get(5)
-      val paramtypeValue = propertyValueList.get(6)
-      val conceptidValue = propertyValueList.get(7)
-
-      model.add(itemRsc, Constants.ITEM_ID_PRP, itemIdValue, XSDDatatype.XSDint)
-      model.add(itemRsc, Constants.LABEL_PRP, labelValue.get, XSDDatatype.XSDstring)
-      model.add(itemRsc, Constants.ABBREVIATION_PRP, abbreviationValue.get, XSDDatatype.XSDstring)
-      model.add(itemRsc, Constants.DB_SOURCE_PRP, dbsourceValue.get, XSDDatatype.XSDstring)
-      model.add(itemRsc, Constants.LINKS_TO_PRP, linkstoValue.get, XSDDatatype.XSDstring)
-      model.add(itemRsc, Constants.CATEGORY_PRP, categoryValue.get, XSDDatatype.XSDstring)
-      model.add(itemRsc, Constants.UNIT_NAME_PRP, unitnameValue.get, XSDDatatype.XSDstring)
-      model.add(itemRsc, Constants.PARAM_TYPE_PRP, paramtypeValue.get, XSDDatatype.XSDstring)
-      model.add(itemRsc, Constants.CONCEPT_ID_PRP, conceptidValue.get, XSDDatatype.XSDint)
-      model.add(itemRsc, RDF.`type`, Constants.MIMIC_D_ITEM)
-    }
-    val queryStr =
-      s"""
-         |PREFIX mimic-rsc:<${Constants.MIMIC_RESOURCE_URI}>
-         |PREFIX mimic-ont:<${Constants.MIMIC_ONTOLOGY_URI}>
-         |SELECT * WHERE {
-         |?itemRsc mimic-ont:itemid ?itemid.
-         |?itemRsc mimic-ont:label ?label.
-         |?itemRsc mimic-ont:abbreviation ?abbreviation.
-         |?itemRsc mimic-ont:dbsource ?dbsource.
-         |?itemRsc mimic-ont:linksto ?linksto.
-         |?itemRsc mimic-ont:category ?category.
-         |?itemRsc mimic-ont:unitname ?unitname.
-         |?itemRsc mimic-ont:paramtype ?paramtype.
-         |?itemRsc mimic-ont:conceptid ?conceptid.
-         |}""".stripMargin
-    val result = QueryingUtils.convertRdf2Result(QueryExecutionFactory.create(queryStr, model).execSelect())
-    Option(result)
+  private def create_ontology_for_d_item(model: Model, key: String, propertyValueList: Option[List[Option[String]]]) = {
+    val itemRsc = ResourceFactory.createResource(Constants.MIMIC_RESOURCE_URI + "d_item/" + key)
+    model.add(itemRsc, Constants.ITEM_ID_PRP, key, XSDDatatype.XSDint)
+    model.add(itemRsc, Constants.LABEL_PRP, propertyValueList.get.head.get, XSDDatatype.XSDstring)
+    model.add(itemRsc, Constants.ABBREVIATION_PRP, propertyValueList.get(1).get, XSDDatatype.XSDstring)
+    model.add(itemRsc, Constants.DB_SOURCE_PRP, propertyValueList.get(2).get, XSDDatatype.XSDstring)
+    model.add(itemRsc, Constants.LINKS_TO_PRP, propertyValueList.get(3).get, XSDDatatype.XSDstring)
+    model.add(itemRsc, Constants.CATEGORY_PRP, propertyValueList.get(4).get, XSDDatatype.XSDstring)
+    model.add(itemRsc, Constants.UNIT_NAME_PRP, propertyValueList.get(5).get, XSDDatatype.XSDstring)
+    model.add(itemRsc, Constants.PARAM_TYPE_PRP, propertyValueList.get(6).get, XSDDatatype.XSDstring)
+    model.add(itemRsc, Constants.CONCEPT_ID_PRP, propertyValueList.get(7).get, XSDDatatype.XSDint)
+    model.add(itemRsc, RDF.`type`, Constants.MIMIC_D_ITEM_CLS)
   }
 
-  def transform_d_lab_items(resultMap: Map[String, Option[Any]]): Option[Result] = {
+
+  /**
+   * ITEMID --> [LABEL, FLUID, CATEGORY, LOINC_CODE]
+   */
+  private def create_ontology_for_d_lab_item(model: Model, key: String, propertyValueList: Option[List[Option[String]]]) = {
+    val itemRsc = ResourceFactory.createResource(Constants.MIMIC_RESOURCE_URI + "d_lab_item/" + key)
+    model.add(itemRsc, Constants.ITEM_ID_PRP, key, XSDDatatype.XSDint)
+    model.add(itemRsc, Constants.LABEL_PRP, propertyValueList.get.head.get, XSDDatatype.XSDstring)
+    model.add(itemRsc, Constants.FLUID_PRP, propertyValueList.get(1).get, XSDDatatype.XSDstring)
+    model.add(itemRsc, Constants.CATEGORY_PRP, propertyValueList.get(2).get, XSDDatatype.XSDstring)
+    model.add(itemRsc, Constants.LOINC_CODE_PRP, propertyValueList.get(3).get, XSDDatatype.XSDstring)
+    model.add(itemRsc, RDF.`type`, Constants.MIMIC_D_LAB_ITEM_CLS)
+  }
+
+
+  /**
+   * ICD9_CODE --> [SHORT_TITLE, LONG_TITLE]
+   */
+  private def create_ontology_for_d_icd_procedures(model: Model, key: String, propertyValueList: Option[List[Option[String]]]) = {
+    val itemRsc = ResourceFactory.createResource(Constants.MIMIC_RESOURCE_URI + "d_icd_procedure/" + key)
+    model.add(itemRsc, Constants.ICD_9_CODE_PRP, key, XSDDatatype.XSDint)
+    model.add(itemRsc, Constants.SHORT_TITLE_PRP, propertyValueList.get.head.get, XSDDatatype.XSDstring)
+    model.add(itemRsc, Constants.LONG_TITLE_PRP, propertyValueList.get(1).get, XSDDatatype.XSDstring)
+    model.add(itemRsc, RDF.`type`, Constants.MIMIC_D_ICD_PROCEDURE_CLS)
+  }
+
+  /**
+   * ICD9_CODE --> [SHORT_TITLE,	LONG_TITLE]
+   */
+  def create_ontology_for_d_icd_diagnoses(model: Model, key: String, propertyValueList: Option[List[Option[String]]]) = {
+    val itemRsc = ResourceFactory.createResource(Constants.MIMIC_RESOURCE_URI + "d_icd_diagnose/" + key)
+    model.add(itemRsc, Constants.ICD_9_CODE_PRP, key, XSDDatatype.XSDint)
+    model.add(itemRsc, Constants.SHORT_TITLE_PRP, propertyValueList.get.head.get, XSDDatatype.XSDstring)
+    model.add(itemRsc, Constants.LONG_TITLE_PRP, propertyValueList.get(1).get, XSDDatatype.XSDstring)
+    model.add(itemRsc, RDF.`type`, Constants.MIMIC_D_ICD_DIAGNOSE_CLS)
+  }
+
+  /**
+   * CGID -->	[LABEL,	DESCRIPTION]
+   */
+  def create_ontology_for_caregivers(model: Model, key: String, propertyValueList: Option[List[Option[String]]]) = {
+    val itemRsc = ResourceFactory.createResource(Constants.MIMIC_RESOURCE_URI + "caregiver/" + key)
+    model.add(itemRsc, Constants.CGID_PRP, key, XSDDatatype.XSDint)
+    model.add(itemRsc, Constants.LABEL_PRP, propertyValueList.get.head.get, XSDDatatype.XSDstring)
+    model.add(itemRsc, Constants.DESCRIPTION_PRP, propertyValueList.get(1).get, XSDDatatype.XSDstring)
+    model.add(itemRsc, RDF.`type`, Constants.MIMIC_CAREGIVER_CLS)
+  }
+
+  def create_ontology_for_procedures_icd(model: Model, key: String, propertyValueList: Option[List[Option[String]]]) = {
     None
   }
 
-  def transform_d_icd_procedures(resultMap: Map[String, Option[Any]]): Option[Result] = {
-    None
-  }
-
-  def transform_d_icd_diagnoses(resultMap: Map[String, Option[Any]]): Option[Result] = {
-    None
-  }
-
-  def transform_caregivers(resultMap: Map[String, Option[Any]]): Option[Result] = {
-    None
-  }
-
-  def transform_drg_codes(resultMap: Map[String, Option[Any]]): Option[Result] = {
-    None
-  }
-
-  def transform_procedures_icd(resultMap: Map[String, Option[Any]]): Option[Result] = {
-    None
-  }
-
-  def transform_diagnoses_icd(resultMap: Map[String, Option[Any]]): Option[Result] = {
+  def create_ontology_for_diagnoses_icd(model: Model, key: String, propertyValueList: Option[List[Option[String]]]) = {
     None
   }
 
   def transformToRdfResult(database: Int, resultMap: Map[String, Option[Any]]): Option[Result] = {
-    database match {
-      case 0 => transform_d_items(resultMap)
-      case 1 => transform_d_lab_items(resultMap)
-      case 2 => transform_d_icd_procedures(resultMap)
-      case 3 => transform_d_icd_diagnoses(resultMap)
-      case 4 => transform_caregivers(resultMap)
-      case 5 => transform_drg_codes(resultMap)
-      case 6 => transform_procedures_icd(resultMap)
-      case 7 => transform_diagnoses_icd(resultMap)
-      case _ => None
+    var query: String = ""
+
+    val model = ModelFactory.createDefaultModel()
+    for ((key, redisResult) <- resultMap) {
+      val propertyValueList = redisResult.asInstanceOf[Option[List[Option[String]]]]
+      database match {
+        case 0 =>
+          create_ontology_for_d_item(model, key, propertyValueList)
+          query = Constants.D_ITEM_QUERY
+        case 1 =>
+          create_ontology_for_d_lab_item(model, key, propertyValueList)
+          query = Constants.D_LAB_ITEM_QUERY
+        case 2 =>
+          create_ontology_for_d_icd_procedures(model, key, propertyValueList)
+          query = Constants.D_ICD_PROCEDURE_QUERY
+        case 3 =>
+          create_ontology_for_d_icd_diagnoses(model, key, propertyValueList)
+          query = Constants.D_ICD_DIAGNOSE_QUERY
+        case 4 =>
+          create_ontology_for_caregivers(model, key, propertyValueList)
+          query = Constants.CAREGIVER_QUERY
+        case 6 =>
+          create_ontology_for_procedures_icd(model, key, propertyValueList)
+          query = Constants.PROCEDURE_ICD_QUERY
+        case 7 =>
+          create_ontology_for_diagnoses_icd(model, key, propertyValueList)
+          query = Constants.DIAGNOSE_ICD_QUERY
+        case _ => println("Invalid database selected. (Valid through 0-7)")
+      }
     }
+    val result = QueryingUtils.convertRdf2Result(QueryExecutionFactory.create(query, model).execSelect())
+    Option(result)
+
 
   }
 }
